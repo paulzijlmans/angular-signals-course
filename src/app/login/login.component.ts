7c1d6e338a46f464d3874648a62980b84ae37e7a
Paul Zijlmans <paul.zijlmans@ordina.nl>
2024-07-11 09:13:35 +0200

    Signal-Based Authentication

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,8 +1,8 @@
-import {Component, inject} from '@angular/core';
-import {Router, RouterLink} from "@angular/router";
-import {AuthService} from "../services/auth.service";
-import {MessagesService} from "../messages/messages.service";
-import {FormBuilder, ReactiveFormsModule} from "@angular/forms";
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from "@angular/router";
+import { AuthService } from "../services/auth.service";
+import { MessagesService } from "../messages/messages.service";
+import { FormBuilder, ReactiveFormsModule } from "@angular/forms";
 
 @Component({
   selector: 'login',
@@ -15,6 +15,28 @@ import {FormBuilder, ReactiveFormsModule} from "@angular/forms";
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  fb = inject(FormBuilder)
+  messagesService = inject(MessagesService)
+  authService = inject(AuthService)
+  router = inject(Router)
 
+  form = this.fb.group({
+    email: [''],
+    password: ['']
+  })
 
+  async onLogin() {
+    try {
+      const { email, password } = this.form.value;
+      if (!email || !password) {
+        this.messagesService.showMessage('Enter an email and password.', 'error')
+        return;
+      }
+      await this.authService.login(email, password)
+      await this.router.navigate(['/home'])
+    } catch (error) {
+      console.error(error)
+      this.messagesService.showMessage('Login failed, please try again.', 'error')
+    }
+  }
 }
